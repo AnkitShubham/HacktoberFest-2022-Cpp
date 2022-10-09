52d6cffe8aabaa3d3ce552b699fac28568db62c0
Ankit Shubham <111640642+AnkitShubham@users.noreply.github.com>
2022-10-09 20:30:06 +0530

    Updated the function to create the pattern.

--- loops/half_pyramid_pattern/main.cpp
+++ loops/half_pyramid_pattern/main.cpp
@@ -2,7 +2,18 @@
 using namespace std;
  
 void half_pyramid_pattern(int n){
-   //Enter your code here
+    int i,j;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i>=j)
+            {
+                cout<<"*";
+            }
+        }
+        cout<<"\n";
+    }
 }
 int main()
 {   
